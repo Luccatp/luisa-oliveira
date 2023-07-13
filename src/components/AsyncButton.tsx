582d4d2e2d60062728d243052512326ff6bb048b
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-07-12 22:33:52 -0300

    fix: imports

--- src/components/AsyncButton.tsx
+++ src/components/AsyncButton.tsx
@@ -1,5 +1,5 @@
 import { ButtonHTMLAttributes, FC, HTMLProps } from 'react';
-import { Button, ButtonProps } from './ui/button';
+import { Button, ButtonProps } from '@/components/ui/button';
 import { Loader2 } from 'lucide-react';
 
 interface AsyncButtonProps extends ButtonProps {
